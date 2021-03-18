56a907058d61e8271e9727bbb60e384b84f17d44
MiltonMMJ <80860093+MiltonMMJ@users.noreply.github.com>
2021-03-17 21:03:19 -0300

    Add files via upload

---
+++ python_reciclagem.py
@@ -0,0 +1,333 @@
+def Historia():
+    print('-=' * 50)
+    print('Aqui falaremos da história resumida dos materias recicláveis')
+    print('-=' * 50)
+    #Papel
+    print(
+    '|' + 'Papel'  + '|', '''A palavra papel vem do latim papyrus e faz referência ao papiro,
+    uma planta que cresce nas margens do rio Nilo no Egito, da qual se
+    extraia fibras para a fabricação de cordas, barcos e as folhas feitas
+    de papiro para a escrita. Quando a escrita surgiu, há mais de 6 mil
+    anos atrás, as palavras eram inscritas em tabuletas de pedras ou
+    argila. A forma mais primitiva de escrita era a cuneiforme. Por volta
+    de 3000 a.C., os egípcios inventaram o papiro.''') 
+    #Metal
+    print('-' * 100)
+    print(
+    '|' + 'Metal' + '|', '''O primeiro metal descoberto foi o cobre,
+    ainda na pré-história, no oriente médio. Coma descoberta deste
+    material e posteriormente de outros metais foi possível desenvolver
+ferramentas mais eficientes que as de pedra.''' )
+    #Plástico
+    print('-' * 100)
+    print(
+    '|' + 'Plástico' + '|', '''A história do plástico começou com Alexander Parkes que, em 1862,
+    descobriu um material orgânico derivado da celulose.
+    Ele buscava um material substituto da borracha, matéria-prima utilizada em muitos produtos na época.''')
+    #Vidro
+    print('-' * 100)
+    print(
+    '|' + 'Vidro' + '|', '''Não se pode afirmar com precisão a data de origem do vidro,
+    mas, com a descoberta de objetos de vidro nas necrópoles egípcias,
+    pode se garantir que ele é conhecido há pelo menos 4 mil anos.''')
+    #Orgânico
+    print('-' * 100)
+    print(
+    '|' + 'Orgânico' + '|', '''No início dos tempos, os primeiros homens eram nômades.
+    Moravam em cavernas, sobreviviam da caça e pesca, vestiam-se de peles e formavam uma 
+    população minoritária sobre a terra. Quando a comida começava a ficar escassa, 
+    eles se mudavam para outra região e os seus "lixos", deixados sobre o meio ambiente, 
+    eram logo decompostos pela ação do tempo.
+    À medida em que foi "civilizando-se" o homem passou a produzir peças para 
+    promover seu conforto: vasilhames de cerâmica, instrumentos para o plantio, roupas mais apropriadas. 
+    Começou também a desenvolver hábitos como construção de moradias, criação de animais, cultivo de alimentos, 
+    além de se fixar  de forma permanente em um local. A produção de lixo consequentemente foi aumentando, 
+    mas ainda não havia se constituído em um problema mundial.''')
+    print('-=' * 50)
+    
+def Reciclavel():
+    print('-=' * 50)
+    print('Aqui falaremos da reciclação')
+    print('-=' * 50)
+     #Papel
+    print(
+    '|' + 'Papel'  + '|', '''A reciclagem do papel é tão importante quanto sua fabricação.
+    A matéria prima para a fabricação do papel já está escassa, mesmo com políticas
+    de reflorestamento e com uma maior conscientização da sociedade em geral.
+    Com o uso dos computadores, muitos cientistas sociais acreditavam que
+    o uso de papel diminuiria, principalmente na indústria e nos escritórios,
+    mas isso não ocorreu e o consumo de papel nas duas últimas décadas do século XX foi
+    recorde. Na fabricação de uma tonelada de papel, a partir de papel usado, o
+    consumo de água é muitas vezes menor e o consumo de energia é cerca da metade.
+    Economizam-se 2,5 barris de petróleo, 98 mil litros de água e 2.500 kw/h
+    de energia elétrica com uma tonelada de papel reciclado.''') 
+    #Metal
+    print('-' * 100)
+    print(
+    '|' + 'Metal' + '|', '''Considerado 100% reciclável, o metal é um produto muito utilizado em nosso dia-a-dia.
+    Esse material pode ser facilmente encontrado em embalagens, fios, aparelhos elétricos,
+    estruturas de construção civil, pontes, veículos, utensílios domésticos, móveis e diversos outros produtos
+    utilizados no nosso cotidiano. Os metais são elementos de grande durabilidade.
+    Uma vez descartados na natureza, o tempo de decomposição de alguns produtos
+    feitos como esse material pode chegar a 500 anos.
+    A reciclagem é muito importante para a indústria de metais.
+    As sucatas são geralmente as principais matérias-primas do processo produtivo e são mais
+    convenientes para o processo de fundição, pois não há perda de qualidade no metal
+    reciclado em relação ao metal produzido com minérios extraídos da natureza.
+    Segundo dados do setor, acredita-se que cerca de 65% do ferro que a indústria utiliza
+    vêm da reciclagem.''' )
+    #Plástico
+    print('-' * 100)
+    print(
+    '|' + 'Plástico' + '|', '''A reciclagem de plástico consiste, basicamente, em três processos:
+    Coleta e separação, revalorização e, por fim, transformação.
+    Coleta e separação: é a separação dos resíduos de acordo com o seu material.
+    Revalorização: é a fase na qual o material já separado passa por um processo que faz com que ele volte a ser matéria-prima.
+    Transformação: fase em que o material transformado em matéria-prima gera um novo produto''')
+    #Vidro
+    print('-' * 100)
+    print(
+    '|' + 'Vidro' + '|', '''A reciclagem do vidro, é um produto 100% reciclável no mundo, ou seja,
+    todo o seu material pode ser reutilizado. Antes do procedimento, é feita uma análise do caco
+    para eliminar materiais impuros que possam estar presentes no produto e que não são recicláveis.
+    Quando o produto é coletado e levado para o local da reciclagem, logo é separado por tipo e cor. 
+    As cores mais encontradas são: translúcido, verde e âmbar.
+    Vale ressaltar que o vidro plano não é reciclado no mesmo local que os outros.''')
+    #Orgânico
+    print('-' * 100)
+    print(
+    '|' + 'Orgânico' + '|', '''Existem duas formas de reciclar o lixo orgânico: a compostagem, gerando adubo para
+    ser usado em plantações, e por meio da produção de biogás.
+    O processo de compostagem pode ser definido como a reciclagem de matéria orgânica.
+    O objetivo é diminuir o impacto nos aterros sanitários, onde toda a matéria vira gás metano,
+    prejudicial quando depositado no solo. É uma alternativa simples, que pode ser feita em casa.
+''')
+    print('-=' * 50) 
+
+def Coordenada():
+    print('-=' * 50)
+    print('''
+    Campinas conta com 16 ecopontos, pontos de entrega voluntária de materiais,
+    nos quais a população pode descartar pequenos volumes de recicláveis 
+    (papel, plástico, vidro e metal), resíduos da construção civil
+    (entulho, madeiras, latas de tinta), pneus, resíduos de poda e jardinagem e
+    objetos inservíveis, como sofás, armários, móveis, entre outros.
+    ''')
+    print('-=' * 50)
+    print('''
+    | 1 |Barão Geraldo      | 2 |Região Central      | 3 |Jardim Pacaembu
+    | 4 |Jardim Eulina      | 5 |Jardim Paranapanema | 6 |Parque Itália
+    | 7 |Parque Itajaí      | 8 |Parque São Jorge    | 9 |Jardim São Gabriel
+    | 10 | Parque Via Norte | 11 |Vida Nova          | 12 |Vila Campos Sales
+    | 13 |Vila União        | 14 |Sousas             | 15 |Carlos Grimaldi
+    | 16 |Jardim Marisa''')
+    print('-' * 100)
+    cord = int(input('Sabendo disso, em qual deles você esta mais perto?'))
+    print('-=' * 50)
+    if cord == 1:
+        print('''
+        Av. Santa Isabel, s/nº
+        Horário de funcionamento: das 7h às 18h, todos os dias, exceto feriados
+        Horário de almoço: das 11h às 12h.''')    
+    elif cord == 2:
+        print('''
+        0 Rua Francisco Theodoro, 1050, Vila Industrial
+        Horário de funcionamento: das 7h às 18h, todos os dias, exceto feriados
+        Horário de almoço: das 11hs às 12hs.''')
+    elif cord == 3:
+        print('''
+        Rua Dante Suriane esq. com Av. Padre Gaspar Bertoni
+        Horário de funcionamento: das 7h às 18h, todos os dias
+        Horário de almoço: das 11h às 12h.''')
+    elif cord == 4:
+        print(''' 
+        Avenida Marechal Rondon esq. Rua José Manuel Veiga
+        Horário de funcionamento: das 7h às 18h, todos os dias
+        Horário de almoço: das 11h às 12h.''')
+    elif cord == 5:
+        print(''' 
+        Rua Serra D água esq. Rua Felismina Stemmer Cajado
+        Horário de funcionamento: das 7h às 18h, todos os dias
+        Horário de almoço: das 11h às 12h.''')
+    elif cord == 6:
+        print(''' 
+        Av. Prefeito Faria Lima, 630, Parque Itália
+        Horário de funcionamento: das 7h às 16h12, de segunda a sexta-feira
+        Horário de almoço: das 11h às 12h.''')
+    elif cord == 7:
+        print(''' 
+        Rua Celso Soares Couto, s/n
+        Horário de funcionamento: das 7h às 18h, todos os dias, exceto feriados
+        Horário de almoço: das 11h às 12h.''')
+    elif cord == 8:
+        print(''' 
+        Rua Plácida Pretini, s/n
+        Horário de funcionamento: das 7h às 18h, todos os dias, exceto feriados
+        Horário de almoço: das 11h às 12h.''')
+    elif cord == 9:
+        print(''' 
+        Rua Jose Martins Lourenço, esq. Rua Geraldo Bretas
+        Horário de funcionamento: das 7h às 18h, todos os dias, exceto feriados
+        Horário de almoço: das 11h às 12h.''')
+    elif cord == 10:
+        print(''' 
+        Rua dos Cambarás, 200
+        Horário de funcionamento: das 7h às 18h, todos os dias, exceto feriados
+        Horário de almoço: das 11h às 12h.''')
+    elif cord == 11:
+        print(''' 
+        Rua Góia Jr. s/n
+        Horário de funcionamento: das 7h às 18h, todos os dias, exceto feriados
+        Horário de almoço: das 11h às 12h.''') 
+    elif cord == 12:
+        print(''' 
+        Avenida São Jose dos Campos, s/n
+        Horário de funcionamento: das 7h às 18h, todos os dias
+        Horário de almoço: das 11h às 12h''')
+    elif cord == 13:
+        print('''
+        Rua Manoel Gomes Ferreira, esq. Rua Jose Ramos Catarino
+        Horário de funcionamento: das 7h às 18h, todos os dias
+        Horário de almoço: das 11h às 12h.''')
+    elif cord == 14:
+        print(''' 
+        Rua Dom Pedro, 464
+        Horário de funcionamento: das 7h às 18h, todos os dias, exceto feriados
+        Horário de almoço: das 11h às 12h.''')
+    elif cord == 15:
+        print(''' 
+        Sobre o Viaduto Manoel M. Carvalho
+        Horário de funcionamento: das 7h às 18h, todos os dias, exceto feriados
+        Horário de almoço: das 11h às 12h.''')
+    elif cord == 16:
+        print(''' 
+        Rua Orlando Bortoletti, 217
+        Horário de funcionamento: das 7h às 19h, todos os dias, exceto feriados
+        Horário de almoço: das 12h às 13h''')
+        
+def Dicas():
+    print('-=' * 35)
+    print('Aqui apresentaremos algumas dicas para você')
+    print('-=' * 35)
+    #Papel
+    print(
+    '|' + 'Papel'  + '|','''Use sempre os dois lados do papel, imprima só quando necessário,
+    procure sempre revisar os textos antes de imprimir, para não gastar muito a toa, prefira o papel 
+    reciclado e o use como rascunho, separe os papéis, caixas e embalagens de papel para a reciclagem.''')
+    #Metal
+    print('-' * 100)
+    print(
+    '|' + 'Metal' + '|', '''Recicle, mas fique atento na hora de separar, pois latas que continham produtos tóxicos,
+    como tintas, não podem ser colocadas na reciclagem com outros tipos de embalagens de metais e procure lavar bem os materiais
+    antes de colocar no lixo ou na reciclagem. ''')
+    #Plástico
+    print('-' * 100)
+    print(
+    '|' + 'Plástico' + '|', '''Use apenas sacolas plásticas recicladas, evite jogar no lixo sacolas ou embalagens plásticas.
+    Potes ou garrafas podem ser usadas como contêineres para alimentos e bebidas separe sempre o material para reciclagem ''')
+    #Vidro
+    print('-' * 100)
+    print(
+    '|' + 'Vidro' + '|', '''O vidro é 100% reciclável, isto é, uma garrafa de vidro pode se transformar em outra garrafa
+    novinha sem perda de qualidade. Recicle sempre e procure separar vidros coloridos de vidros transparentes. ''')
+    #Orgânico
+    print('-' * 100)
+    print(
+    '|' + 'Orgânico' + '|', '''A solução é compostagem, devolvendo assim os nutrientes para o solo.
+    Reutilize como adubo para seu jardim. Evite também cozinhar ou comprar comida em excesso e  
+    estando sempre atento aos prazos de validade.''')
+    print('-=' * 50)
+    
+def Reu():
+    print('-=' * 35)
+    print('Aqui falaremos de algumas formas de reutilizações para você')
+    print('-=' * 35)
+     #Papel
+    print(
+    '|' + 'Papel'  + '|', '''Limpe vidros e espelhos com os jornais velhos, que já foram lidos e estão com notícias desatualizadas,
+    podem ser utilizados para limpar vidros e espelhos. Faça bloquinhos o ideal é utilizar sempre os dois lados do papel,
+    mas caso precise imprimir algo somente de um lado, você pode reaproveitar o lado limpo montando bloquinhos de anotação.
+    Faça anéis para guardanapos os rolos das toalhas de papel e dos papéis higiênicos podem ser transformados 
+    em excelentes anéis para guardanapo. ''')
+    #Metal
+    print('-' * 100)
+    print(
+    '|' + 'Metal' + '|', '''A sucata esses resíduos, quando destinados às tratadoras, podem ser lucrativos, pois há determinadas 
+    sucatas com grande potencial de reaproveitamento. Cobre um dos grandes benefícios para tratadoras que lidam com os
+    resíduos de cobre é que o metal é 100% reciclável.  O cobre é dos poucos materiais que podem ser reciclados diversas 
+    vezes sem qualquer perda de desempenho ou de qualidade. Porta lápis reutilizar latas de metal para fazer um porta lápis. ''')
+    #Plástico
+    print('-' * 100)
+    print(
+    '|' + 'Plástico' + '|', '''Porta lápis funcional: Sabe aqueles galões plásticos que sobram  após serem muito bem lavados,
+    você pode fazer recortes em uma das partes laterais e criar um porta lápis que é totalmente funcional.
+    Porta revistas:
+    Suas garrafas PET podem se tornar ótimos porta revistas. Após limpar as garrafas que serão reutilizadas, 
+    cole-as em sequência em uma base de plástico. Após isso, recorte a parte de cima de cada garrafa e passe uma 
+    fita no corte para evitar futuros acidentes.
+    Casinhas para pássaros:
+    Corte uma garrafa em duas partes, sendo que você pode descartar o meio. Na parte debaixo, recorte um retângulo, 
+    lá será a portinha da casa. Depois, é só colar as duas metades e decorar da forma que você preferir.''')
+    #Vidro
+    print('-' * 100)
+    print(
+    '|' + 'Vidro' + '|', '''Porta velas  para decoração é outra utilidade dos potes de vidro para um design diferenciado
+    Frascos de requeijão ou chocolate em creme, por exemplo, podem ser reaproveitados como copos. Recipientes de vidro, 
+    geralmente transparentes, são ótimos para serem utilizados como potes organizadores. Os pequenos frascos, 
+    facilmente guardados em gavetas ou colocados em prateleiras, podem armazenar miudezas
+    como parafusos, pregos, agulhas, botões, miçangas, entre outros. ''')
+    #Orgânico
+    print('-' * 100)
+    print(
+    '|' + 'Orgânico' + '|', '''A compostagem transforma o lixo orgânico em adubo e pode ser feita em casa. Há dois tipos de compostagem:
+    a tradicional, feita apenas com a decomposição do material orgânico, e a feita com minhocas, conhecida como minhocasa.
+    Enterramento:
+    Esse processo é indicado para situações em que a produção de resíduos orgânicos é pequena ou para situações em que há grandes
+    espaços de terra. Isso porque para realizar o enterramento de forma correta sem produzir mal cheio ou danificar o solo
+    é necessário um espaço isolado e adequado. ''')
+    print('-=' * 50)
+    
+def invalid():
+    print('-' * 50)
+    print('Número inválido / Tente novamente')
+    print('-' * 50)
+    
+def inicio():
+    print('-=' * 50)
+    print("| 1 | História", "| 2 |Recicláveis", "| 3 |Coordenada", "| 4 |Dicas", "| 5 |Reutilização", "| 0 |Sair ")
+    print('-=' * 50)
+    
+def escolha():
+    loop  = int(input("Digite as opções: "))
+    escolha = loop
+    return escolha
+    
+    
+#Menu Principal
+inicio()
+valor = escolha()
+while valor != 0: 
+#História / Menu
+    
+    if valor == 1:
+         Historia()
+         
+#Recicláveis / Menu    
+    elif valor == 2:
+        Reciclavel()
+#Coordenada / Menu
+    elif valor == 3:
+        Coordenada()
+#Dicas / Menu    
+    elif valor == 4:
+        Dicas()
+#Reutilização  / Menu
+    elif valor == 5:
+        Reu()
+#Se não for nenhuma das opções
+    else:
+        invalid()
+    inicio()    
+    valor = escolha()
+    
+    
